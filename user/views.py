89bcb2085c6a558ab641575cac4de48924e9bdc6
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-12 16:25:50 +0900

    Feat : 회원정보 수정 및 회원탈퇴 생성
    1. 회원정보 수정에 필요한 instance 값을 넘겨주기 위해 user를 UserSerializer에 같이 넣어 보내줘야 한다.

--- user/views.py
+++ user/views.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
 
+from user.models import User as UserModel
+
 
 class UserView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -19,4 +21,26 @@ class UserView(APIView):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-   
+    # 회원정보 수정
+    def put(self, request):
+        user = UserModel.objects.get(id=request.user.id)
+        
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # 회원탈퇴
+    def delete(self, request):
+        user = request.user
+        
+        if user:
+            user.delete()
+            return Response({"message": "회원탈퇴 성공"}, status=status.HTTP_200_OK)
+        
+        return Response({"message": "회원탈퇴 실패"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    
