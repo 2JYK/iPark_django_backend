e23fd6f9f4fd1829cf60e9ddf2e286b06eabb698
GyeongSuKim <rla04722@naver.com>
2022-07-12 20:28:35 +0900

    Feat : CommunityApp urls.py
    community,comment 작성

--- ipark/urls.py
+++ ipark/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from community import views
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('community/', views.CommunityView.as_view()),
+    path('community/', include("community.urls")),
 ]
