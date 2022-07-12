92a5bcc48803f5870e6a3bafbc534b38666aa14b
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-12 16:24:00 +0900

    Feat : 회원가입 api 작성

--- user/views.py
+++ user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class UserView(APIView):
+    authentication_classes = [JWTAuthentication]
+    
+    # 회원가입
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+   
