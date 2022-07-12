913156d9dc409d7a7fccd62124316441fd32c618
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-12 18:35:39 +0900

    Feat : user의 아이디를 찾는 기능 작성
    1. 정규표현식을 사용해 먼저 올바른 양식에 맞게 사용자가 작성했는지 필터링 진행
    2. 해당 값이 옳지 않다면 에러 메세지를 보내줌
    3. 해당 값이 옳다면 쿼리를 날려 해당 user의 username을 찾고 반환해줌

--- user/views.py
+++ user/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.db.models import Q
+import re
 
 from user.serializers import UserSerializer
 
@@ -44,3 +46,20 @@ class UserView(APIView):
         return Response({"message": "회원탈퇴 실패"}, status=status.HTTP_400_BAD_REQUEST)
     
     
+class FindUserInfoView(APIView):
+    # 아이디 찾기
+    def post(self, request):
+        correct_email = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+        correct_phone = re.compile('\d{3}-\d{4}-\d{4}')
+        
+        email_input = correct_email.match(request.data["email"])
+        phone_input = correct_phone.match(request.data["phone"])
+        
+        if email_input == None or phone_input == None:
+            return Response({'msg': '이메일 혹은 핸드폰 번호 양식이 올바르지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            searched_username = UserModel.objects.get(Q(email=request.data["email"]) & Q(phone=request.data["phone"])).username
+        
+        if searched_username:
+            return Response({"username" : searched_username}, status=status.HTTP_200_OK)
+        
