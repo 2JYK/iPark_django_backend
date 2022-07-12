703bf7278cb4e223c50e7bbfe3a8668508c8d482
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-12 21:48:18 +0900

    Feat : 공원 상세페이지 댓글 작성 구현
    공원 상세 정보 조회 코드 수정
    filter -> get

--- park/views.py
+++ park/views.py
@@ -3,12 +3,44 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from park.models import Park as ParkModel
+
 from park.serializers import ParkDetailSerializer
+from park.serializers import ParkCommentSerializer
 
 
 class ParkView(APIView):
     # 공원 상세 정보 조회
     def get(self, request, park_id):
-        park = ParkModel.objects.filter(id=park_id)
-        serialized_data = ParkDetailSerializer(park, many=True).data
+        park = ParkModel.objects.get(id=park_id)
+        serialized_data = ParkDetailSerializer(park).data
         return Response(serialized_data, status=status.HTTP_200_OK)
+    
+
+# 공원 상세 페이지의 댓글창    
+class ParkCommentView(APIView):
+    # 댓글 작성
+    def post(self, request, park_id):
+        park = ParkModel.objects.get(id=park_id)
+        
+        data = {
+            "user" : request.user.id,
+            "park" : park.id,
+            "comment" : request.data["comment"]
+        }
+        
+        comment_serializer = ParkCommentSerializer(data=data)
+
+        if comment_serializer.is_valid():
+            comment_serializer.save()
+            return Response(comment_serializer.data, status=status.HTTP_200_OK)
+        
+        return Response({"message": "댓글 작성을 실패하였습니다"}, status=status.HTTP_200_OK)
+    
+    # 댓글 수정
+    def put(self, request, comment_id):
+        return Response({})
+    
+    # 댓글 삭제
+    def delete(self, request):
+        return Response({})
+    
