9258524071ea08dd42b8782b3a77c17a2df54a43
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-12 16:22:13 +0900

    Feat : UserSerializer 생성
    1. 회원가입에 사용되며, custom validator와 custom creator를 사용한다.
    2. custom validator를 통해 중요 항목에 조건을 달아 제대로 된 양식의 값만 받도록 한다.
    3. custom creator를 통해 비밀번호를 해싱해준다.

---
+++ user/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+
+from user.models import User as UserModel
+
+
+EMAIL = ("@naver.com", "@gmail.com", "@kakao.com")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = UserModel
+        fields = ["username", "password", "fullname", "email", "phone", "birthday", "region", "join_date"]
+        
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+        
+    def validate(self, data):
+        if not len(data.get("username", "")) >= 6:
+            raise serializers.ValidationError(
+                detail={"error": "username의 길이는 6자리 이상이어야 합니다."}
+            )
+            
+        if not data.get("email", "").endswith(EMAIL):
+            raise serializers.ValidationError(
+                detail={"error": "네이버, 구글, 카카오 이메일만 가입할 수 있습니다."}
+            )
+            
+        if not len(data.get("password", "")) >= 6:
+            raise serializers.ValidationError(
+                detail={"error": "password의 길이는 6자리 이상이어야 합니다."}
+            )
+            
+        return data
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', '')
+        
+        user = UserModel(**validated_data)
+        user.set_password(password)
+        user.save()
+        
+        return user
+        
+    
+    
