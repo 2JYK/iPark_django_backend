9b373fbcee53705b4628a7e94307977cd0ab75bb
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-12 23:47:59 +0900

    Feat : 공원 댓글 기능 구현
    1. 댓글 작성 / 수정 / 삭제 기능구현
    2. urls 생성

--- park/urls.py
+++ park/urls.py
@@ -3,5 +3,7 @@ from park import views
 
 
 urlpatterns = [
-    path('<park_id>/', views.ParkView.as_view())
+    path('<park_id>/', views.ParkView.as_view()),
+    path('<park_id>/comment/', views.ParkCommentView.as_view()),
+    path('<park_id>/comment/<comment_id>/', views.ParkCommentView.as_view())
 ]
--- park/views.py
+++ park/views.py
@@ -3,8 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from park.models import Park as ParkModel
+from park.models import ParkComment as ParkCommentModel
 
 from park.serializers import ParkDetailSerializer
+from park.serializers import ParkCommentSerializer
 
 
 class ParkView(APIView):
@@ -14,3 +16,43 @@ class ParkView(APIView):
         serialized_data = ParkDetailSerializer(park).data
         
         return Response(serialized_data, status=status.HTTP_200_OK)
+    
+
+# 공원 상세 페이지의 댓글창    
+class ParkCommentView(APIView):
+    # 댓글 작성
+    def post(self, request, park_id):
+        park = ParkModel.objects.get(id=park_id)
+        
+        data = {
+            "user" : request.user.id,
+            "park" : park.id,
+            "comment" : request.data["comment"]
+        }
+        
+        comment_serializer = ParkCommentSerializer(data=data)
+
+        if comment_serializer.is_valid():
+            comment_serializer.save()
+            return Response(comment_serializer.data, status=status.HTTP_200_OK)
+        
+        return Response({"message" : "댓글 작성이 실패되었습니다"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    # 댓글 수정
+    def put(self, request, park_id, comment_id):
+        comment = ParkCommentModel.objects.get(id=comment_id)
+        
+        comment_serializer = ParkCommentSerializer(comment, data=request.data, partial=True)
+
+        if comment_serializer.is_valid():
+            comment_serializer.save()
+            return Response(comment_serializer.data, status=status.HTTP_200_OK)
+        
+        return Response({"message" : "댓글 수정이 실패되었습니다"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    # 댓글 삭제
+    def delete(self, request, park_id, comment_id):
+        comment = ParkCommentModel.objects.get(id=comment_id)
+        comment.delete()
+        
+        return Response({"message" : "해당 댓글이 삭제되었습니다"}, status=status.HTTP_200_OK)
