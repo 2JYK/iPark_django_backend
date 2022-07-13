1e1ab259160d11a84051065539939517985bb9a9
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-14 00:04:47 +0900

    Style : iPark urls.py 따옴표 수정

--- ipark/urls.py
+++ ipark/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('park/', include('park.urls'))
-    path('user/', include('user.urls')),
-    path('community/', include("community.urls")),
+    path("admin/", admin.site.urls),
+    path("park/", include("park.urls")),
+    path("user/", include("user.urls")),
+    path("community/", include("community.urls")),
 ]
