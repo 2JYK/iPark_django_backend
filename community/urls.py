5046c7af6f114ab7cf19175b894562a7fd198a16
GyeongSuKim <rla04722@naver.com>
2022-07-13 15:31:38 +0900

    Feat : CommunityApp urls.py
    comment url

--- community/urls.py
+++ community/urls.py
@@ -3,5 +3,7 @@ from community import views
 
 
 urlpatterns = [
-    path("", views.CommunityView.as_view()),
+    path("", views.CommunityView.as_view(), name="community_view"),
+    path("comment/", views.CommentView.as_view()),
+    path("comment/<comment_id>", views.CommentView.as_view()),
 ]
