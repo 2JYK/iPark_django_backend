dbd9da38f3f8bf9302c1a3b613015009141dfe74
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-07-13 18:39:40 +0900

    Fix : 오타수정

--- ipark/urls.py
+++ ipark/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('park/', include('park.urls'))
+    path('park/', include('park.urls')),
     path('user/', include('user.urls')),
     path('community/', include("community.urls")),
 ]
