94574cc64418cff72b74a8b1176aaae1d35c023d
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-13 14:45:44 +0900

    Feat : 로그인 테스트 진행
      테스트 진행 시 print(response.data["access"])를 통해 access 토큰 값을 확인할 수 있음

--- user/tests.py
+++ user/tests.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+
+from user.models import User
 from user.models import Region
 
 
@@ -27,3 +29,21 @@ class UserRegistrationTest(APITestCase):
 
         self.assertEqual(response.status_code, 200)
         
+
+# 로그인 테스트
+class UserLoginTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_data = {"username" : "user10", "password" : "101010"}
+        cls.user = User.objects.create_user("user10", "101010")
+        
+    def test_login(self):
+        url = reverse("token_obtain_pair")
+        user_data = {
+            "username" : "user10",
+            "password" : "101010"
+        }
+        
+        response = self.client.post(url, user_data)
+        
+        self.assertEqual(response.status_code, 200)
