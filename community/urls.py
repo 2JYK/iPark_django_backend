d7a28cc364bf8a3af34ab815b1e4351a0ad34283
GyeongSuKim <rla04722@naver.com>
2022-07-13 15:06:24 +0900

    Feat : serializers, views, url
    Serializer : ArticleSerializer 작성
    
    views : CommunityView 작성
    
    url : 커뮤니티 url 연결

---
+++ community/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from community import views
+
+
+urlpatterns = [
+    path("", views.CommunityView.as_view()),
+]
--- community/views.py
+++ community/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from community.serializers import ArticleSerializer
+
+from community.models import Article as ArticleModel
+
+
+class CommunityView(APIView):
+    def get(self, request):
+        article = ArticleModel.objects.all().order_by('-created_at')
+        serialized_data = ArticleSerializer(article, many=True).data
+
+        return Response(serialized_data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        request.data["user"] = request.user.id
+        article_serializer = ArticleSerializer(data=request.data)
+
+        if article_serializer.is_valid():
+            article_serializer.save()
+            return Response(article_serializer, status=status.HTTP_200_OK)
+        return Response(article_serializer, status=status.HTTP_400_BAD_REQUEST)
