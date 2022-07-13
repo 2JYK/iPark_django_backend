279ff923692c1668be0557259e975f3829810266
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-13 14:33:21 +0900

    Feat :회원가입 테스트 진행
    1. setUpTestData()를 사용해 TestCase가 끝날 때까지 region 데이터 유지

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from user.models import Region
 
-# Create your tests here.
+
+# 회원가입 테스트
+class UserRegistrationTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.region_data = {"region_name": "강남구", "region_name": "강동구", "region_name": "강북구"}
+        cls.region = Region.objects.create(**cls.region_data)
+        
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username" : "user10",
+            "password" : "101010",
+            "fullname" : "user10",
+            "email" : "user10@gmail.com",
+            "phone" : "010-1010-1010",
+            "birthday" : "2022-07-13",
+            "region" : 1
+        }
+        
+        response = self.client.post(url, user_data)
+
+        self.assertEqual(response.status_code, 200)
+        
