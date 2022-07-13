5f8c90a33b750019820dc5ea1d47ae0eee414732
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-13 17:16:07 +0900

    Feat : custom validator에 정규표현식 적용
    이메일, 핸드폰번호, 비밀번호, 생년월일에 정규표현식을 설정해 회원가입을 할 때 사용자가 값을 잘못 입력하면 에러 메세지로 사용자가 가입할 수 있도록 안내함

--- user/serializers.py
+++ user/serializers.py
@@ -1,9 +1,10 @@
+import re
 from rest_framework import serializers
 
 from user.models import User as UserModel
 
 
-EMAIL = ("@naver.com", "@gmail.com", "@kakao.com")
+EMAIL = ("@naver.com", "@gmail.com", "@kakao.com", "daum.net", "nate.com", "outlook.com")
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -17,21 +18,40 @@ class UserSerializer(serializers.ModelSerializer):
         }
         
     def validate(self, data):
+        correct_email = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+        correct_phone = re.compile("(010)-\d{4}-\d{4}")
+        correct_password = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")
+        correct_birthday = re.compile("\d{4}-\d{2}-\d{2}$")
+                
+        email_input = correct_email.match(data.get("email", ""))
+        phone_input = correct_phone.match(data.get("phone", ""))
+        password_input = correct_password.match(data.get("password", ""))
+        birthday_input = correct_birthday.match(str(data.get("birthday", "")))
+        
         if data.get("username"):
             if not len(data.get("username", "")) >= 6:
                 raise serializers.ValidationError(
-                    detail={"error": "username의 길이는 6자리 이상이어야 합니다."}
-                )
+                    detail={"error": "username의 길이는 6자리 이상이어야 합니다."})
                 
-        if not data.get("email", "").endswith(EMAIL):
+        if email_input == None:
             raise serializers.ValidationError(
-                detail={"error": "네이버, 구글, 카카오 이메일만 가입할 수 있습니다."}
-            )
+                detail={"error": "이메일 형식이 올바르지 않습니다."})
+        else:
+            if not data.get("email", "").endswith(EMAIL):
+                raise serializers.ValidationError(
+                    detail={"error": "네이버, 구글, 카카오, 다음, 네이트, 아웃룩 이메일만 가입할 수 있습니다."})
+        
+        if password_input == None:
+            raise serializers.ValidationError(
+                detail={"error": "비밀번호는 8 자리 이상이며 최소 하나 이상의 영문자, 숫자, 특수문자가 필요합니다."})
+        
+        if phone_input == None:
+            raise serializers.ValidationError(
+                detail={"error": "전화번호는 010-0000-0000 형식으로 작성해주시기 바랍니다."})
             
-        if not len(data.get("password", "")) >= 6:
+        if birthday_input == None:
             raise serializers.ValidationError(
-                detail={"error": "password의 길이는 6자리 이상이어야 합니다."}
-            )
+                detail={"error": "생년월일은 YYYY-MM-DD 형식으로 작성해주시기 바랍니다."})
             
         return data
         
