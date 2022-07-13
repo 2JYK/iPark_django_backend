ee5a763db6fab81ad6884e37236504fb1d03b078
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-13 16:17:40 +0900

    Feat : 아이디 찾기 테스트 진행을 위한 url name 생성

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,5 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("", views.UserView.as_view(), name="user_view"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("myid/", views.FindUserInfoView.as_view()),
+    path("myid/", views.FindUserInfoView.as_view(), name="myid_view"),
 ]
