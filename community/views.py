43a27a5af64fccf2943e6b666568e71d356a6f07
juhyun1011 <juhyun4567@gmail.com>
2022-07-13 15:17:03 +0900

    Feat : 게시글 태그별 불러오기

--- community/views.py
+++ community/views.py
@@ -6,17 +6,20 @@ from rest_framework.views import APIView
 from rest_framework import status
 
 from community.serializers import ArticleSerializer
-
 from community.models import Article as ArticleModel
 
 
 class CommunityView(APIView):
     def get(self, request):
-        article = ArticleModel.objects.all().order_by('-created_at')
-        serialized_data = ArticleSerializer(article, many=True).data
-
-        return Response(serialized_data, status=status.HTTP_200_OK)
-
+        if request.id == 1:
+            article = ArticleModel.objects.filter(tag=1).order_by('-created_at')
+            serialized_data = ArticleSerializer(article, many=True).data
+            return Response(serialized_data, status=status.HTTP_200_OK)
+        else:
+            article = ArticleModel.objects.filter(tag=2).order_by('-created_at')
+            serialized_data = ArticleSerializer(article, many=True).data
+            return Response(serialized_data, status=status.HTTP_200_OK)
+            
     def post(self, request):
         request.data["user"] = request.user.id
         article_serializer = ArticleSerializer(data=request.data)
