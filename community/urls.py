321492696576f2a4770285e8a0a4ce9e93507b21
juhyun1011 <juhyun4567@gmail.com>
2022-07-13 21:47:07 +0900

    Fix : community url int 지정

--- community/urls.py
+++ community/urls.py
@@ -4,7 +4,7 @@ from community import views
 
 urlpatterns = [
     path("", views.CommunityView.as_view(), name="community_view"),
-    path("<article_id>/", views.CommunityDetailView.as_view()),
-    path("<article_id>/comment/", views.CommentView.as_view()),
-    path("comment/<comment_id>/", views.CommentView.as_view()),
+    path("<int:article_id>/", views.CommunityDetailView.as_view()),
+    path("<int:article_id>/comment/", views.CommentView.as_view()),
+    path("comment/<int:comment_id>/", views.CommentView.as_view()),
 ]
