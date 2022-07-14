15b5aa8792ad30b2adc011437e2a7d866367e28d
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-14 17:44:01 +0900

    Feat : 공원 조회수별 검색 url 생성

--- park/urls.py
+++ park/urls.py
@@ -4,6 +4,7 @@ from park import views
 
 urlpatterns = [
     path("search/", views.ParkSearchView.as_view(), name="park_search"),
+    path("popularity/", views.ParkPopularityView.as_view(), name="park_popularity"),
     path("<park_id>/", views.ParkView.as_view(), name="park"),
     path("<park_id>/comment/", views.ParkCommentView.as_view(), name="park_comment_create"), 
     path("<park_id>/comment/<comment_id>/", views.ParkCommentView.as_view(), name="park_comment_manage"),
