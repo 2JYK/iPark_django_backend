9b04a4843bcb45f283e6301bf184d36d23ef1851
eversrx19 <eversrx19@gmail.com>
2022-07-14 09:52:06 +0900

    change import ordering

--- user/views.py
+++ user/views.py
@@ -1,9 +1,9 @@
+import re
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.db.models import Q
-import re
 
 from user.serializers import UserSerializer
 
