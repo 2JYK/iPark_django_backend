be6b27aae11ffad9692dca70b1bc6224093b81eb
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-14 23:36:59 +0900

    Update : url 이름 수정

--- park/urls.py
+++ park/urls.py
@@ -3,7 +3,7 @@ from park import views
 
 
 urlpatterns = [
-    path("search/", views.ParkSearchView.as_view(), name="park_search"),
+    path("option/", views.OptionView.as_view(), name="park_search"),
     path("popularity/", views.ParkPopularityView.as_view(), name="park_popularity"),
     path("<park_id>/", views.ParkView.as_view(), name="park"),
     path("<park_id>/comment/", views.ParkCommentView.as_view(), name="park_comment_create"), 
