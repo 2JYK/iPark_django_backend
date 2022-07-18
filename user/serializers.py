69d0b66e06c2441c443f1fd20a1db2be27835355
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-18 22:35:12 +0900

    Feat : 회원정보 수정 serializer 구현

--- user/serializers.py
+++ user/serializers.py
@@ -62,3 +62,44 @@ class UserSerializer(serializers.ModelSerializer):
         
         return instance
     
+    
+class accountUpdateSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = UserModel
+        fields = ["username", "password", "fullname", "email",
+                  "phone", "birthday", "region", "join_date"]
+
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def validate(self, data):
+        correct_phone = re.compile("(010)-\d{4}-\d{4}")
+
+        phone_input = correct_phone.match(data.get("phone", ""))
+
+        if data.get("username"):
+            if not len(data.get("username", "")) >= 6:
+                raise serializers.ValidationError(
+                    detail={"error": "username의 길이는 6자리 이상이어야 합니다."})
+
+        if not data.get("email", "").endswith(EMAIL):
+            raise serializers.ValidationError(
+                detail={"error": "네이버, 구글, 카카오, 다음, 네이트, 아웃룩 이메일만 가입할 수 있습니다."})
+
+        if phone_input == None:
+            raise serializers.ValidationError(
+                detail={"error": "전화번호는 010-0000-0000 형식으로 작성해주시기 바랍니다."})
+
+        return data
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            if key == "password":
+                instance.set_password(value)
+                continue
+            setattr(instance, key, value)
+        instance.save()
+
+        return instance
