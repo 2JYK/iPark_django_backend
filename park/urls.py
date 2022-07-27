e90d6b457155c4361579f1f2e3f2e9279d82a250
juhyun1011 <juhyun4567@gmail.com>
2022-07-27 16:01:10 +0900

    Feat : 공원 즐겨 찾기 기능 추가

--- park/urls.py
+++ park/urls.py
@@ -5,6 +5,7 @@ from park import views
 urlpatterns = [
     path("", views.ToggleParkView.as_view(), name="toggle_park"),
     path("option/", views.OptionView.as_view(), name="park_search"),
+    path("bookmark/", views.BookMarkView.as_view(), name="bookmark"),
     path("popularity/", views.ParkPopularityView.as_view(), name="park_popularity"),
     path("<park_id>/", views.ParkView.as_view(), name="park"),
     path("<park_id>/comment/", views.ParkCommentView.as_view(), name="park_comment_create"), 
