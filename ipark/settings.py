69cdbba2c710f2fee557eab96793ed8384260bee
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-28 17:22:56 +0900

    Feat : static, media 설정

--- ipark/settings.py
+++ ipark/settings.py
@@ -205,11 +205,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 
-
-MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
