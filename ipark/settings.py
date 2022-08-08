d10f124e3f1b18e9d8a88078b054ba7136095bd3
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-08-08 21:37:31 +0900

    Feat : 커뮤니티 이미지 사이즈 100M로 상향수정

--- ipark/settings.py
+++ ipark/settings.py
@@ -229,6 +229,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 100000000
+FILE_UPLOAD_MAX_MEMORY_SIZE = 100000000
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
