d4c7357b303dc737cd5f0135d3b2ada9bfca9e48
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-09 17:14:01 +0900

    Feat : custom jwt url 생성
    - 기존과 다르게 로그인을 하면 이 url로 들어와 프론트로 username이 담긴 토큰을 전달해준다.
    - 기존의 url 삭제

--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from user import views
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+    TokenRefreshView
 )
 
 urlpatterns = [
     path("", views.UserView.as_view(), name="user_view"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh_pair"),
+    path('api/ipark/token/', views.iParkTokenObtainPairView.as_view(), name='ipark_token'),
     path("kakao/", views.KakaoLoginView.as_view(), name="kakao"),
     path("myid/", views.FindUserInfoView.as_view(), name="myid_view"),
     path("alterpassword/", views.AlterPasswordView.as_view(), name="alter_password_view"),
