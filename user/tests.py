7e5d009a054b1f211fb97aa62dbc83418fedf69f
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-11 23:03:10 +0900

    Feat : 중복 username 사용시 회원가입 테스트

--- user/tests.py
+++ user/tests.py
@@ -65,6 +65,33 @@ class UserRegistrationTest(APITestCase):
         
         response = self.client.post(url, user_data)
 
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["username"][0], "username의 길이는 6자리 이상이어야 합니다.")
         
-    
+    # 중복 username일 때의 회원가입 테스트
+    def test_registration_same_username(self):
+        url = reverse("user_view")
+        user_data = {
+            "username" : "user10",
+            "password" : "1010abc!",
+            "fullname" : "user10",
+            "email" : "user10@gmail.com",
+            "phone" : "010-1010-1010",
+            "region" : 2
+        }
+        
+        user_data_2 = {
+            "username" : "user10",
+            "password" : "2020abc!",
+            "fullname" : "user20",
+            "email" : "user20@gmail.com",
+            "phone" : "010-2020-2020",
+            "region" : 2
+        }
+        
+        response = self.client.post(url, user_data)
+        response_2 = self.client.post(url, user_data_2)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response_2.status_code, 400)
+        self.assertEqual(response_2.data["username"][0], "user의 사용자 계정은/는 이미 존재합니다.")
