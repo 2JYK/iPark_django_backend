3071028d5e0b5e9db377e2551fabad60df62a495
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-12 14:18:48 +0900

    Feat : password가 없을 때의 회원정보 수정 테스트

--- user/tests.py
+++ user/tests.py
@@ -478,4 +478,25 @@ class UserInfoModifyDeleteTest(APITestCase):
         
         self.assertEqual(response.status_code, 201)
     
+    # password가 없을 때
+    def test_modify_no_password(self):
+        url = reverse("user_view")
+        data_for_change = {
+            "username" : "user20",
+            "password" : "",
+            "fullname" : "user20",
+            "email" : "user20@gmail.com",
+            "phone" : "010-1010-1010",
+            "region" : 4
+        }
+        
+        response = self.client.put(
+            path=url, 
+            data=data_for_change,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
+        )
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["password"][0], "이 필드는 blank일 수 없습니다.")
+        
     
